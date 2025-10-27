d37416e5c6f6914f71f2de6ed4477ebcbc2ee511
Krishnendu Nayak <krishnedunayakbsc24@rvu.edu.in>
2025-10-27 12:46:21 +0530

    employee hello added

--- sample.java
+++ sample.java
@@ -1,6 +1,7 @@
 class sample{
     public static void main(String args[]){
-        System.out.println("Hello Students");
+        //System.out.println("Hello Students");
+        System.out.println("Hello Employees");
            }
 
 }
