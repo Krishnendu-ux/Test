9b81661e8d58a3fe2a7da9a00713f83077461c92
aadxarsh <aadarshbhandaribsc24@rvu.edu.in>
2025-10-27 12:47:58 +0530

    Add greeting message for mam

--- sample.java
+++ sample.java
@@ -2,6 +2,7 @@ class sample{
     public static void main(String args[]){
         //System.out.println("Hello Students");
         System.out.println("Hello Employees");
+         System.out.println("hiii mam ");
            }
 
 }
