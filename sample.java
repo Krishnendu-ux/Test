b6b868c5562800b40511ea7188fe2e2124478b70
aadxarsh <aadarshbhandaribsc24@rvu.edu.in>
2025-10-28 21:44:21 +0530

    joke
    lol lol lol soo funyyyyy mannnn!!

--- sample.java
+++ sample.java
@@ -1,6 +1,18 @@
 class sample{
     public static void main(String args[]){
         //System.out.println("Hello Students");
+           String[] employees = { "Boss Baby", "Coffee Addict", "Spreadsheet Samurai", "Meeting Ninja", "WiFi Whisperer" };
+        String[] reactions = {
+            "just asked for a raise using interpretive dance.",
+            "debugged the printer by yelling at it.",
+            "scheduled a meeting to discuss why we have too many meetings.",
+            "brought donuts... but only for themselves.",
+            "replied-all to a company-wide email with 'unsubscribe'."
+        };
+
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i] + " " + reactions[i]);
+        }
         System.out.println("Hello Employees");
          System.out.println("hiii mam ");
            }
